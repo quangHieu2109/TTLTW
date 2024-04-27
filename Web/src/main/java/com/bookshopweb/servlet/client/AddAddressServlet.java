9d89178a4e8ded646a7d3b8a636cf3ebfe767d4a
Hai000 <21130343@st.hcmuaf.edu.vn>
2024-04-27 14:55:44 +0700

    create form address.jsp, create class AddAddressServlet.java

---
+++ Web/src/main/java/com/bookshopweb/servlet/client/AddAddressServlet.java
@@ -0,0 +1,16 @@
+package com.bookshopweb.servlet.client;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AddAddressServlet", value = "/addAddress")
+public class AddAddressServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/address.jsp").forward(req, resp);
+    }
+}
